1404a389d988324345afa3bedc2d6f4c5642c428
Namit <pareeknamit8@gmail.com>
2026-05-18 22:52:24 +0530

    fix: Add crypto import to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const crypto = require('crypto');
 require('dotenv').config();
 
 const app = express();
